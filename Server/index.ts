2c5a6bc82ddbb2432c57e9d2ee2c420ce1fdc493
eeeeeenye <mayosung113@hanmail.net>
2023-10-12 18:18:41 +0900

    add : Server

---
+++ Server/index.ts
@@ -0,0 +1,34 @@
+exports {}
+
+require('dotenv').config()
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+const cors = require('cors')
+const path = require('path')
+const env = require('dotenv')
+
+env.config({path: path.resolv(__dirname, './config/config.js')})
+
+/* 포트설정 */
+app.set('port',process.env.PORT||8080)
+
+/* 공통 미들웨어 */
+app.use(logger('dev'))
+app.use(express.json())
+app.use(
+    cors({
+        origin: ['http://localhost:19006']
+    })
+)
+
+app.get('/', (req,res) => {
+    res.status(200).send("접속완료되었습니다.")
+})
+
+/*라우터 코드 추가 */
+
+/* 서버 포트 연결 */
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'),'번에서 서버 실행 중...')
+})

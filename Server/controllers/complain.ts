5908af9b11873722b64d679c55e2b00b1ffd7541
eeeeeenye <mayosung113@hanmail.net>
2023-10-20 18:32:38 +0900

    add: server

--- Server/controllers/complain.ts
+++ Server/controllers/complain.ts
@@ -1,10 +1,26 @@
 const db = require('../db')
 const path = require('path')
 
+const database = db.db('inhye')
 
 module.exports = {
     // 민원 게시글 관리 컨트롤러
-    complainComtrol: async(req,res) => {
-        
+    
+    // 민원 게시글 읽기 -> 시별 민원 게시글 확인
+    complainRead: async(req,res) => {
+        try{
+            const city = req.body.city;
+
+        }catch(error){
+
+        }
+    },
+
+    // 민원 게시글 해결 시 게시글 삭제
+    complainRead: async(req,res) => {
+        try{
+        }catch(error){
+
+        }
     }
 }

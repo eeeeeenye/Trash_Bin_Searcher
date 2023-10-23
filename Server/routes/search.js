859af6864ec7a2da17f1dce638313256d2397b12
eeeeeenye <mayosung113@hanmail.net>
2023-10-23 14:48:48 +0900

    add: routes

--- Server/routes/search.js
+++ Server/routes/search.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
-const controller = require('../controllers/search')
+const controller = require('../controllers/search.ts')
 
-router.post('/bin_read_region',controller.RegionCotrol)     // 지역별 쓰레기통 데이터 처리
-router.post('/bin_read_myloc',controller.MylocControl)      // 내 위치 기반 쓰레기통 데이터 처리
+router.post('/bin_read_region', controller.regionControl); // 지역별 쓰레기통 데이터 처리
+router.post('/bin_read_myloc', controller.myLocControl); // 내 위치 기반 쓰레기통 데이터 처리
 
 module.exports = router

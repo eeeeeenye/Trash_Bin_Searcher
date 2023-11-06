ed95f453b1369564e7768a6bf54efca2159278db
eeeeeenye <mayosung113@hanmail.net>
2023-11-06 18:27:35 +0900

    add: server

--- Server/routes/post.js
+++ Server/routes/post.js
@@ -1,4 +1,4 @@
-const router = require('express').Router
+const router = require('express').Router();
 const controller = require('../controllers/post.ts')
 
 router.post('/bin_post',controller.postingController)
